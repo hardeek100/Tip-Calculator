abd28d88767204582790e2a82324f2e1fb95104a
Hardik <hp@Hs-Mac-mini.local>
2021-08-14 13:16:38 -0500

    first commit

---
+++ Tip Calculator/Tip Calculator/SettingsViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SettingsViewController.swift
+//  Tip Calculator
+//
+//  Created by H P on 8/14/21.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    
+    @IBOutlet weak var first: UITextField!
+    @IBOutlet weak var second: UITextField!
+    @IBOutlet weak var third: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Settings"
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
